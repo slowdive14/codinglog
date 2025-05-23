ac4bd9d523354c22ab053a86900889592db0e9e0
slowdive14 <100013214+slowdive14@users.noreply.github.com>
2025-05-24 00:20:35 +0900

    Title: 세 실수의 반올림, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250524/세 실수의 반올림/rounding-of-three-actual-numbers.py
@@ -0,0 +1,9 @@
+a = float(input())
+b = float(input())
+c = float(input())
+formatted_number1 = f"{a:.3f}"
+formatted_number2 = f"{b:.3f}"
+formatted_number3 = f"{c:.3f}"
+print(formatted_number1)
+print(formatted_number2)
+print(formatted_number3)
